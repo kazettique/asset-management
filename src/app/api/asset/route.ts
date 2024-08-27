b4f35ed22273dd0bc94ec65ba567c189c7afc5b0
Inori Inuyama <kazettique@Inoris-MacBook.home>
2024-08-27 17:42:06 +0800

    test asset query

---
+++ src/app/api/asset/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server';
+
+import { db } from '@/lib/db';
+
+export async function GET(_request: Request) {
+  const rawData = await db.asset.findMany({
+    select: {
+      brand: {
+        select: { name: true },
+      },
+      comment: true,
+      endCurrency: {
+        select: { display: true, symbol: true },
+      },
+      endDate: true,
+      endMethod: {
+        select: { name: true, type: true },
+      },
+      endPrice: true,
+      id: true,
+      isCensored: true,
+      isUsed: true,
+      meta: true,
+      name: true,
+      startCurrency: {
+        select: { display: true, symbol: true },
+      },
+      startDate: true,
+      startMethod: {
+        select: { name: true, type: true },
+      },
+      startPrice: true,
+    },
+  });
+
+  return NextResponse.json(rawData);
+}
